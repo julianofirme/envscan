8e3fe3d2648c0845d841c162982ddda0f72b74e9
Juliano <julianofirme23@gmail.com>
2024-08-13 17:20:35 -0300

    feat: generate the report in json

--- go.mod
+++ go.mod
@@ -36,5 +36,6 @@ require (
 require (
 	github.com/go-git/go-git/v5 v5.12.0
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- packages/report/report.go
+++ packages/report/report.go
@@ -2,33 +2,29 @@ package report
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
-	"path/filepath"
-	"time"
 )
 
 func GenerateReport(matches []string, format string) error {
-	report := map[string]interface{}{
-		"timestamp": time.Now().Format(time.RFC3339),
-		"matches":   matches,
+	if format != "json" {
+		return nil
 	}
 
-	var output []byte
-	var err error
-	switch format {
-	case "json":
-		output, err = json.MarshalIndent(report, "", "  ")
-	case "html":
-		// TODO: implement HTML report
-	case "csv":
-		// TODO: implement CSV report
-	default:
-		return fmt.Errorf("unknown format: %s", format)
+	report := map[string][]string{
+		"secrets": matches,
 	}
+
+	file, err := os.Create("secrets-report.json")
 	if err != nil {
 		return err
 	}
+	defer file.Close()
 
-	return os.WriteFile(filepath.Join("reports", fmt.Sprintf("report_%d.%s", time.Now().Unix(), format)), output, 0644)
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(report); err != nil {
+		return err
+	}
+
+	return nil
 }
